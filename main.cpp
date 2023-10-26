89a63de76022183b769fd87e80e277dc06c8e949
Charity <charity.grey1@gmail.com>
2023-10-25 21:08:30 -0700

    added local time detection

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
-#include <iostream>
+#include <iostream> //input and output stream
+#include <unistd.h>
+#include <ctime> //time
+#include <fstream> //filestream
 
 using namespace std;
 
@@ -6,9 +9,14 @@ int main() {
    bool x = true;
    string introduction = "Welcome to the Rate Limiter Simulator (c) 2023 Charity Grey\nTo exit, please type \'!exit\'.";
    string user_input = "";
+   time_t recorded_time;
+   struct tm * timeinfo;
    cout << introduction << endl;
    while (x) {
       cin >> user_input;
+      cout << time(&recorded_time) << " seconds has passed since 00:00 GMT, Jan 1, 1970" << endl;
+      timeinfo = localtime(&recorded_time);
+      cout << asctime(timeinfo) << endl;
       if (user_input == "!exit") {
          x = false;
       }
